ef04e460a1deed74e89d8a75e31e6369bcc38f9e
atallah11111 <febryandavakok@gmail.com>
2024-05-06 10:09:30 +0700

    modul7 commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,7 @@ Route::get('/', function () {
 });
 
 Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('welcome', [HomeController::class, 'index'])->name('welcome');
 
 Route::get('profile', ProfileController::class)->name('profile');
 Route::resource('employees', EmployeeController::class);
